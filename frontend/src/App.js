96669f59884a87f1cce5f10274afac2a4e8cb3fe
d3nnyyy <d3nnn41k@gmail.com>
2023-07-25 18:26:42 +0300

    changed url in post request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,21 +8,21 @@ import { useState } from "react";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import { TextField } from "@mui/material";
-import {Button} from "@mui/material"
+import { Button } from "@mui/material"
 import LinearProgress from '@mui/material/LinearProgress'
 import { createTheme, ThemeProvider } from "@mui/material";
 export const muiTheme = createTheme({
   components: {
-      MuiCheckbox: {
-          styleOverrides: {
-              root: {
-                  color: '#9c27b0',
-                  '&.Mui-checked': {
-                    color: '#9c27b0', // Set the desired color when the checkbox is checked
-                  },
-              },
+    MuiCheckbox: {
+      styleOverrides: {
+        root: {
+          color: '#9c27b0',
+          '&.Mui-checked': {
+            color: '#9c27b0', // Set the desired color when the checkbox is checked
           },
+        },
       },
+    },
   },
 });
 function App() {
@@ -67,10 +67,10 @@ function App() {
       genres.push('drama, ')
     }
     if (fantasyChecked) {
-      genres.push('fantasy, ') 
+      genres.push('fantasy, ')
     }
     if (historicalChecked) {
-      genres.push('historical, ') 
+      genres.push('historical, ')
     }
     if (horrorChecked) {
       genres.push('horror, ')
@@ -97,29 +97,29 @@ function App() {
     return res
   }
 
-  
+
 
   const submitParams = async () => {
     setIsLoading(true)
     setIsMovie(false)
     console.log(isLoading)
-    await axios.post('http://localhost:8080/movies/movie', 
-    {
-      rating:`${rating[0]}-${rating[1]}`,
-      releaseYear:`${year[0]}-${year[1]}`,
-      filmLength:`${length[0]}-${length[1]}`,
-      genre:gatherGenres(),
-      desiredCast:`${cast}`
-    }
+    await axios.post('http://movie-generator-env2.eba-aas3v2ez.eu-central-1.elasticbeanstalk.com/',
+      {
+        rating: `${rating[0]}-${rating[1]}`,
+        releaseYear: `${year[0]}-${year[1]}`,
+        filmLength: `${length[0]}-${length[1]}`,
+        genre: gatherGenres(),
+        desiredCast: `${cast}`
+      }
     ).then(response => {
       setMovie(response.data)
-      setTimeout(function(){setIsLoading(false); setIsMovie(true)}, 1000)
+      setTimeout(function () { setIsLoading(false); setIsMovie(true) }, 1000)
     })
-    .catch(error => {
-      console.log(error);
-    })
-    
-    
+      .catch(error => {
+        console.log(error);
+      })
+
+
   }
   const handleRatingChange = (event, newValue) => {
     setRating(newValue);
@@ -183,7 +183,7 @@ function App() {
     <div className="App">
       <div className={`${isLoading ? 'loading' : 'hidden'}`}>
         <h1 className="loader-title">Loading...</h1>
-        <LinearProgress color="secondary"/>
+        <LinearProgress color="secondary" />
       </div>
       <div className={`${isMovie ? 'movieShowcase' : 'hidden'}`}>
         <h1>Your movie:</h1>
@@ -233,36 +233,36 @@ function App() {
         <div className="genre-container">
           <h3>Choose the genre of the film:</h3>
           <div className="checkbox-container">
-          <ThemeProvider theme={muiTheme}>
+            <ThemeProvider theme={muiTheme}>
               <FormGroup>
-              <FormControlLabel control={<Checkbox checked={actionChecked} onChange={handleActionChange}/>} label="Action" color="primary"/>
-              <FormControlLabel control={<Checkbox checked={adventureChecked} onChange={handleAdventureChange}/>} label="Adventure" color="secondary"/>
-              <FormControlLabel control={<Checkbox checked={animatedChecked} onChange={handleAnimatedChange} />} label="Animated" color="secondary"/>
-              <FormControlLabel control={<Checkbox checked={comedyChecked} onChange={handleComedyChange}/>} label="Comedy" color="secondary"/>
+                <FormControlLabel control={<Checkbox checked={actionChecked} onChange={handleActionChange} />} label="Action" color="primary" />
+                <FormControlLabel control={<Checkbox checked={adventureChecked} onChange={handleAdventureChange} />} label="Adventure" color="secondary" />
+                <FormControlLabel control={<Checkbox checked={animatedChecked} onChange={handleAnimatedChange} />} label="Animated" color="secondary" />
+                <FormControlLabel control={<Checkbox checked={comedyChecked} onChange={handleComedyChange} />} label="Comedy" color="secondary" />
               </FormGroup>
-            <FormGroup>
-            <FormControlLabel control={<Checkbox checked={dramaChecked} onChange={handleDramaChange}/>} label="Drama" color="secondary"/>
-              <FormControlLabel control={<Checkbox checked={fantasyChecked} onChange={handleFantasyChange}/>} label="Fantasy" color="secondary"/>
-              <FormControlLabel control={<Checkbox checked={historicalChecked} onChange={handleHistoricalChange}/>} label="Historical" color="secondary"/>
-              <FormControlLabel control={<Checkbox checked={horrorChecked} onChange={handleHorrorChange}/>} label="Horror" color="secondary"/>
-            </FormGroup>
-            <FormGroup>
-            <FormControlLabel control={<Checkbox checked={musicalChecked} onChange={handleMusicalChange}/>} label="Musical" color="secondary"/>
-            <FormControlLabel control={<Checkbox checked={noirChecked} onChange={handleNoirChange}/>} label="Noir" color="secondary"/>
-            <FormControlLabel control={<Checkbox checked={romanceChecked} onChange={handleRomanceChange}/>} label="Romance" color="secondary"/>
-        
-            </FormGroup>
-            <FormGroup>
-            <FormControlLabel control={<Checkbox checked={sciencefictionChecked} onChange={handleScienceFictionChange}/>} label="Science Fiction" color="secondary"/>
-            <FormControlLabel control={<Checkbox checked={thrillerChecked} onChange={handleThrillerChange}/>} label="Thriller" color="secondary"/>
-            <FormControlLabel control={<Checkbox checked={westernChecked} onChange={handleWesternChange}/>} label="Western" color="secondary"/>
-            </FormGroup>
+              <FormGroup>
+                <FormControlLabel control={<Checkbox checked={dramaChecked} onChange={handleDramaChange} />} label="Drama" color="secondary" />
+                <FormControlLabel control={<Checkbox checked={fantasyChecked} onChange={handleFantasyChange} />} label="Fantasy" color="secondary" />
+                <FormControlLabel control={<Checkbox checked={historicalChecked} onChange={handleHistoricalChange} />} label="Historical" color="secondary" />
+                <FormControlLabel control={<Checkbox checked={horrorChecked} onChange={handleHorrorChange} />} label="Horror" color="secondary" />
+              </FormGroup>
+              <FormGroup>
+                <FormControlLabel control={<Checkbox checked={musicalChecked} onChange={handleMusicalChange} />} label="Musical" color="secondary" />
+                <FormControlLabel control={<Checkbox checked={noirChecked} onChange={handleNoirChange} />} label="Noir" color="secondary" />
+                <FormControlLabel control={<Checkbox checked={romanceChecked} onChange={handleRomanceChange} />} label="Romance" color="secondary" />
+
+              </FormGroup>
+              <FormGroup>
+                <FormControlLabel control={<Checkbox checked={sciencefictionChecked} onChange={handleScienceFictionChange} />} label="Science Fiction" color="secondary" />
+                <FormControlLabel control={<Checkbox checked={thrillerChecked} onChange={handleThrillerChange} />} label="Thriller" color="secondary" />
+                <FormControlLabel control={<Checkbox checked={westernChecked} onChange={handleWesternChange} />} label="Western" color="secondary" />
+              </FormGroup>
             </ThemeProvider>
             <div></div>
           </div>
         </div>
         <div className="cast-container">
-        <h3>Input the desired cast for the movie:</h3>
+          <h3>Input the desired cast for the movie:</h3>
           <TextField color="secondary" className="textField" id="outlined-basic" label="Input the desired cast..." variant="outlined" value={cast} onChange={(event) => setCast(event.target.value)} />
         </div>
         <div className="button-container">
@@ -270,9 +270,9 @@ function App() {
         </div>
       </div>
       <div className={`${isMovie ? 'buttons-container' : 'hidden'}`}>
-          <ColorButton variant="contained" size="Medium" onClick={submitParams}>Regenerate</ColorButton>
-          <ColorButton variant="contained" size="Medium" onClick={function(){setIsMovie(false)}}>Edit parameters</ColorButton>
-        </div>
+        <ColorButton variant="contained" size="Medium" onClick={submitParams}>Regenerate</ColorButton>
+        <ColorButton variant="contained" size="Medium" onClick={function () { setIsMovie(false) }}>Edit parameters</ColorButton>
+      </div>
     </div>
   );
 }
