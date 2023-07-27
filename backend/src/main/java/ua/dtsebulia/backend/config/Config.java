8da6586f6461879b7469d82d8aa5ec5a2fa4bed4
d3nnyyy <d3nnn41k@gmail.com>
2023-07-27 16:08:46 +0300

    updated config file

--- backend/src/main/java/ua/dtsebulia/backend/config/Config.java
+++ backend/src/main/java/ua/dtsebulia/backend/config/Config.java
@@ -1,18 +1,20 @@
 package ua.dtsebulia.backend.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ssm.SsmClient;
+import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
+import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
 
 @Configuration
 public class Config {
 
-    @Value("${key}")
-    String key;
-
     @Bean
     public RestTemplate restTemplate() {
+        String key = getSecretKeyFromParameterStore();
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getInterceptors().add((request, body, execution) -> {
             request.getHeaders().add("Authorization", "Bearer " + key);
@@ -21,4 +23,17 @@ public class Config {
         return restTemplate;
     }
 
+    private String getSecretKeyFromParameterStore() {
+        SsmClient ssmClient = SsmClient.builder()
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.EU_CENTRAL_1)
+                .build();
+
+        GetParameterRequest parameterRequest = GetParameterRequest.builder()
+                .name("/movie-generator-key")
+                .build();
+
+        GetParameterResponse parameterResponse = ssmClient.getParameter(parameterRequest);
+        return parameterResponse.parameter().value();
+    }
 }
